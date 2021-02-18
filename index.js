4ee4d74f492dd78978697ce5ea304327cf4ef90f
ef1301 <fangemily60@gmail.com>
2021-02-18 18:18:55 -0500

    completion of filter

--- index.js
+++ index.js
@@ -9,9 +9,20 @@ const myEach = function(arr,callback){
 
 //Map Implementation
 const myMap = function(arr,callback){
-  let new_array = []; // initialize array
+  let new_array = []; // initialize array to be returned
   for(let i = 0; i < arr.length; i++){ // traverse the parameter array
     new_array.push(callback(arr[i])); // push returned callback value to new_array
+  }
+  return new_array; // return the new array
+}
+
+//filter Implementation
+const myFilter = function(arr,callback){
+  let new_array = []; // initialize array to be returned
+  for(let i = 0; i < arr.length; i++){ // traverse the parameter array
+    if(callback(arr[i]) === true){ // Callback returns true
+      new_array.push(arr[i]); // push matched arr[i] to new array
+    }
   }
   return new_array; // return the new array
 }
