ae1379636486909361ed6084762f9216394c89a4
ef1301 <fangemily60@gmail.com>
2021-02-18 19:28:12 -0500

    modified function names to match assignment

--- index.js
+++ index.js
@@ -18,13 +18,13 @@ Array.prototype.myEach = function(callback){
 }
 
 /*
-   Map() calls a provided callback function once for each element in an array, in order, and constructs a new array from the results.
+   myMap() calls a provided callback function once for each element in an array, in order, and constructs a new array from the results.
 
    callback is invoked only for indexes of the array which have assigned values (including undefined).
 
-   Map() returns a new array containing the results of calling a function on every element in this array.
+   myMap() returns a new array containing the results of calling a function on every element in this array.
  */
-Array.prototype.Map = function(callback){
+Array.prototype.myMap = function(callback){
   let new_array = []; // initialize new array
 
   for(let i = 0; i < this.length; i++){
@@ -35,15 +35,15 @@ Array.prototype.Map = function(callback){
 }
 
 /*
-   Filter() calls a provided callback function once for each element in an array, and constructs a new array of all the values for which callback returns a value that coerces to true.
+   myFilter() calls a provided callback function once for each element in an array, and constructs a new array of all the values for which callback returns a value that coerces to true.
 
    callback is invoked only for indexes of the array which have assigned values; it is not invoked for indexes which have been deleted or which have never been assigned values.
    Array elements which do not pass the callback test are skipped, and are not included in the new array.
 
-   Filter returns a new array with the elements that pass the test.
+   myFilter returns a new array with the elements that pass the test.
    If no elements pass the test, an empty array will be returned.
  */
-Array.prototype.Filter = function(callback){
+Array.prototype.myFilter = function(callback){
   let new_array = [];
 
   for(let i = 0; i < this.length; i++){
